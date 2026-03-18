c705de66a24747270dfa574d10b951281c814fcb
giovaville <giovannivillegas4556@gmail.com>
2026-03-18 00:07:40 -0600

    commit2

---
+++ PROYECTO1_IPC1/src/proyecto1_ipc1/PROYECTO1_IPC1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1_ipc1;
+
+/**
+ *
+ * @author Gio
+ */
+public class PROYECTO1_IPC1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
